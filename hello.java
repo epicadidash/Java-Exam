169189d2e5286a5dd3a3ea2d2c8ca9af0ad8068e
epicadidash <dradwaitdash@gmail.com>
2023-03-22 00:58:19 +0530

    Raw Code

--- hello.java
+++ hello.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter any Two numbers");
+        int k = Sc.nextInt();
+        int l = Sc.nextInt();
+        int maxg =1;
+        for(int i=2;i<Math.max(l, k);i++)
+        {
+            if(k%i==0&&l%i==0&&i<maxg)
+            {
+                maxg=i;
+            }
+        }
+        System.out.println("GCD:"+maxg);
     }
 }
