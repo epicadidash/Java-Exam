0581ba0b8d67d955ceb024de6bd15e327d4be89b
epicadidash <dradwaitdash@gmail.com>
2023-03-22 01:04:16 +0530

    For Testing2: Success

--- hello.java
+++ hello.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class hello {
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
-        System.out.println("Hello");
         System.out.println("Enter any Two numbers");
         int k = Sc.nextInt();
         int l = Sc.nextInt();
