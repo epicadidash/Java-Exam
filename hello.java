154762fa8834ee06bd4afafd854000e72adf84ae
epicadidash <dradwaitdash@gmail.com>
2023-03-22 01:03:39 +0530

    For Testing1: Failed

--- hello.java
+++ hello.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 public class hello {
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
+        System.out.println("Hello");
         System.out.println("Enter any Two numbers");
         int k = Sc.nextInt();
         int l = Sc.nextInt();
         int maxg =1;
-        for(int i=2;i<Math.max(l, k);i++)
+        for(int i=2;i<=Math.max(l, k);i++)
         {
-            if(k%i==0&&l%i==0&&i<maxg)
+            if(k%i==0&&l%i==0&&i>maxg)
             {
                 maxg=i;
             }
